bc50500e186bf52f94ab5ec5c57b6be627183d72
balakrishnanthirumal <balakrishnanthirumalt@gmail.com>
2025-02-27 09:44:04 +0530

    DOne

--- background.js
+++ background.js
@@ -2,25 +2,24 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (!message.url) return;
 
   fetch(
-    "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyAbBlQVU7qJrskzFX5C0cgiOvVpsavlXaY",
+    "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=YOUR_API_KEY",
     {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        client: { clientId: "your-extension", clientVersion: "1.0" },
         threatInfo: {
           threatTypes: ["SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE", "MALWARE"],
           platformTypes: ["ANY_PLATFORM"],
           threatEntryTypes: ["URL"],
-          treatEntries: [{ url: message.url }],
+          threatEntries: [{ url: message.url }],
         },
       }),
     }
   )
     .then((response) => response.json())
     .then((data) => {
-      if (data.result === true) {
+      if (data.matches) {
         chrome.scripting.executeScript({
           target: { tabId: sender.tab.id },
           files: ["popup.js"],
