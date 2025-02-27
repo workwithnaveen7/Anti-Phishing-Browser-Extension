cd9810f52156a98eb8876b855f06cec1d2ac2af4
balakrishnanthirumal <balakrishnanthirumalt@gmail.com>
2025-02-27 09:45:44 +0530

    Done

--- background.js
+++ background.js
@@ -2,12 +2,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (!message.url) return;
 
   fetch(
-    "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=YOUR_API_KEY",
+    "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyAbBlQVU7qJrskzFX5C0cgiOvVpsavlXaY",
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        client: { clientId: "your-extension", clientVersion: "1.0" },
         threatInfo: {
           threatTypes: ["SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE", "MALWARE"],
           platformTypes: ["ANY_PLATFORM"],
